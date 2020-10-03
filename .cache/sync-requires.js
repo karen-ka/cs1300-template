62362b0dc834524b9bd4b7ea07f0a55599380d9e
Karen Ka <karen925@gmail.com>
2020-10-03 03:25:17 -0400

    run build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/node_modules/gatsby-plugin-offline/app-shell.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/index.js"))),
   "component---src-pages-persona-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/persona.js")))
