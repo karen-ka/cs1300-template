f257f3fb3ac08b2edd66262e655cdbdc26c0c403
Karen Ka <karen925@gmail.com>
2020-10-03 03:11:55 -0400

    remove unused pages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,10 +7,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/404.js"))),
-  "component---src-pages-imprint-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/imprint.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/index.js"))),
-  "component---src-pages-persona-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/persona.js"))),
-  "component---src-pages-privacy-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/privacy.js"))),
-  "component---src-pages-project-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/project.js")))
+  "component---src-pages-persona-js": hot(preferDefault(require("/Users/karenka/Desktop/CS/uiux/cs1300-template/src/pages/persona.js")))
 }
 
